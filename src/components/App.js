a3f48d2f5eb5ccc485e7315525093baf25e433a0
Clair Wanjiku <clair.karanja@student.moringaschool.com>
2023-10-26 09:00:36 +0300

    completed assignment

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Keypad from "./Keypad";
+import EyesOnMe from "./EyesOnMe";
+
+function App() {
+    return (
+        <div>
+            <Keypad />
+            <EyesOnMe />
+        </div>
+    )
+}
